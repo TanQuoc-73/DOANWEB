6fa2eaa69dd4206617b284c1d8be8e3a7012f2cd
Dương Quốc Tần <cutorasusu2004@Gmail.com>
2025-03-11 23:00:13 +0700

    node+react

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const port = PORT||3000;
 
 app.use(express .json());
 
 
 app.get ('/api/home', (req, res) => {
-    
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
 })
